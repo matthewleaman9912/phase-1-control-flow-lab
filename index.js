a4dd542972ed21a13100f794d51753c689510035
MatthewLeaman <leaman.30@osu.edu>
2023-12-11 21:54:58 -0500

    complete control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let someValue;
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400) {
+    const freeRide = "This one is on me!"
+    return freeRide;
+  }
+  else if (someValue > 400 & someValue < 2000) {
+    const twentyRide = "That will be twenty bucks."
+    return twentyRide;
+  }
+  else if (someValue >= 2000 & someValue <= 2500) {
+    const thirtyRide = "I will gladly take your thirty bucks."
+    return thirtyRide;
+  }
+  else {
+    const noRide = "No can do."
+    return noRide;
+  }
 }
-
-function ternaryCheckCity(){
-  // Write your code here!
+let someCity;
+function ternaryCheckCity(someCity){
+  if (someCity === "NYC") {
+    const goodToGo = "Ok, sounds good."
+    return goodToGo;
+  }
+  else {
+    const noGo = "No go."
+    return noGo;
+  }
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+let tipLuck;
+function switchOnCharmFromTip(tipLuck){
+  if (tipLuck === "generous") {
+    const greatTip = "Thank you so much."
+    return greatTip;
+  }
+  else if (tipLuck === "not as generous") {
+    const goodTip = "Thank you."
+    return goodTip;
+  }
+  else {
+    const badTip = "Bye."
+    return badTip;
+  }
 }
